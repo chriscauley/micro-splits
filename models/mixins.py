5f09a5954509dfcfeb2068d8c17cce3ddb0d12ec
chriscauley <chris@lablackey.com>
2022-07-25 15:43:13 -0400

    first

---
+++ models/mixins.py
@@ -0,0 +1,75 @@
+import numpy as np
+import time
+
+import urcv
+
+
+class WaitKeyMixin:
+    def __init__(self, *args, **kwargs):
+        self._index = 0
+        self.goto = None
+        self._goto_delta = None
+        self._last_tick = time.time()
+
+    def increase_goto_by(self, amount):
+        self.goto_sign = np.sign(amount)
+        self.goto = self._index + amount
+        self.goto = self.goto % self.get_max_index()
+
+    @property
+    def busy(self):
+        return self.goto != None
+
+    def wait_key(self):
+        if self.goto is not None:
+            delta = self.goto - self._index
+            if not self._goto_delta:
+                self._goto_delta = delta
+                self._goto_start = time.time()
+            if abs(delta) > 10 and self._index % 100 == 99:
+                self._last_tick = time.time()
+            self._index += self.goto_sign
+            if self.goto == self._index:
+                self.goto = None
+                ms = int(1000 * (time.time() - self._goto_start))
+                if ms > 1:
+                    # TODO whether or not to print should be controled by a flag
+                    print(f'{self._goto_delta} frames took {ms}ms')
+                self._goto_delta = None
+            self._index = self._index % self.get_max_index()
+            return
+        key = urcv.wait_key()
+        if key == 'right':
+            self._index += 1
+        elif key == 'left':
+            self._index -= 1
+        elif key == 'down':
+            self.increase_goto_by(-10)
+        elif key == 'up':
+            self.increase_goto_by(10)
+        elif key == ',':
+            self.increase_goto_by(-100)
+        elif key == '.':
+            self.increase_goto_by(100)
+        elif key == '<':
+            self.increase_goto_by(-1000)
+        elif key == '>':
+            self.increase_goto_by(1000)
+        elif key == '0':
+            self._index = 0
+        elif key == '/':
+            self.increase_goto_by(self.get_max_index() - self._index - 1)
+            print("Goint to end:", self.goto)
+        elif key == 'g':
+            while True:
+                s = input("Go to what index?")
+                if s.isdigit():
+                    self.increase_goto_by(int(s) - self._index)
+                    break
+        else:
+            return key
+        self._index = self._index % self.get_max_index()
+
+
+    def get_max_index(self):
+        raise NotImplementedError()
---
+++ models/video.py
@@ -0,0 +1,64 @@
+import cv2
+
+from .mixins import WaitKeyMixin
+
+GAME_WIDTH = 300
+GAME_HEIGHT = 224
+GAME_SHAPE = (GAME_WIDTH, GAME_HEIGHT)
+HUD_SPLIT = 31
+
+class Video(WaitKeyMixin):
+    """
+    Class for interacting with a video (mkv, mp4, etc)
+    """
+    def __init__(self, file_path):
+        super().__init__()
+        self.file_path = file_path
+        self.cap = cv2.VideoCapture(file_path)
+        self._index = -1 # foces next line to load frame
+        self._cached_index = None
+        self.get_frame(0)
+
+    def get_frame(self, target_index=None):
+        if target_index == None:
+            target_index = self._index
+        if self._cached_index != target_index:
+            if target_index == 0:
+                # opencv is 1 indexed, so we'll just return the first frame
+                # this means the first and second frames are going to be the same
+                target_index = 1
+            self._cached_index = target_index
+
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, target_index)
+            ret, self._frame_image = self.cap.read()
+            self._raw_image = self._frame_image
+            current_shape = self._frame_image.shape
+            if current_shape[:2] != GAME_SHAPE:
+                self._frame_image = cv2.resize(
+                    self._frame_image,
+                    GAME_SHAPE,
+                    interpolation=cv2.INTER_NEAREST
+                )
+            if not ret:
+                raise NotImplementedError("Video is not loaded")
+        self._index = target_index
+        return self._frame_image
+
+    def get_hud_content(self):
+        return self.get_frame()[:HUD_SPLIT]
+
+    def get_game_content(self):
+        return self.get_frame()[HUD_SPLIT:]
+
+    def get_current_time(self):
+        return self.cap.get(cv2.CAP_PROP_POS_MSEC)
+
+    def get_max_index(self):
+        return self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    def increase_goto_by(self, amount):
+        if amount < 0:
+            self._index += amount
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, self._index)
+        else:
+            super().increase_goto_by(amount)
