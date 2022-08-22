fdac220a4c3e3a6d7e47982201a06844e9b73895
chriscauley <chris@lablackey.com>
2022-08-22 10:10:16 -0400

    get_data function

--- models/__init__.py
+++ models/__init__.py
@@ -1,20 +1,2 @@
-import json
-import numpy as np
-from pathlib import Path
-from unrest.utils import JsonCache
-
+from .get_data import get_data
 from .video import Video
-
-class NumpyEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        if isinstance(obj, np.integer):
-            return int(obj)
-        return json.JSONEncoder.default(self, obj)
-
-
-def get_data(video_path):
-    Path('.data').mkdir(exist_ok=True)
-    data = JsonCache(f'.data/{video_path.split("/")[-1]}.json', __encoder__=NumpyEncoder)
-    return data
